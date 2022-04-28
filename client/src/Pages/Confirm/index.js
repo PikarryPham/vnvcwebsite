e3830487e67d9d5edbd156e6233cba354b37afd0
[nt2505] <nth250520@gmail.com>
2022-04-28 09:11:57 +0700

    Align text and add navigation

--- client/src/Pages/Confirm/index.js
+++ client/src/Pages/Confirm/index.js
@@ -7,8 +7,14 @@ import { useParams } from 'react-router-dom';
 import { tongTien } from "../../utils";
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function Confirm({ match }) {
+  let navigate = useNavigate();
+
+  const backToVaccinePage = () => {
+    navigate("/vaccine");
+  }
 
   const { id } = useParams();
 
@@ -26,7 +32,7 @@ export default function Confirm({ match }) {
     <div>
       <Header />
       <StepRegister current={2} />
-      <div>
+      <div style={{marginLeft: "180px"}}>
         <div style={{display: "inline-flex"}}>
           <Container 
                     style={{
@@ -44,69 +50,72 @@ export default function Confirm({ match }) {
                         backgroundImage: `url('https://d2jx2rerrg6sh3.cloudfront.net/image-handler/ts/20211119101350/ri/950/src/images/news/ImageForNews_697293_1637334825213259.jpg')`,
                         height: 250, 
                         backgroundRepeat: 'no-repeat',
-                        backgroundPosition: 'center'}}>
+                        backgroundPosition: 'center',
+                        }}>
                             
       </Container>
-        <h4>VNVC chỉ thực hiện giữ chỗ cho vắc xin sau khi nhận được thanh toán từ Quý khách và tổng số lượng đặt mua không vượt quá giới hạn mũi tiêm chúng tôi có thể cung cấp.</h4>
-        <div>
-          Mã đặt mua : {data?.MaDatMua}
-        </div>
-
-        <div>
-          Số người đăng ký : {data?.NguoiTiem?.length}
-        </div>
-
-        <div>
-          Tổng số tiền cần thanh toán : {tongTien(data?.NguoiTiem).toLocaleString()} VNĐ
-        </div>
-
-        <h3 style = {{fontWeight: 'bold'}}>Hướng dẫn thanh toán</h3>
-
-        <div>
-          1. Quý khách có thể thanh toán trực tiếp tại các Trung tâm VNVC trên toàn quốc (Danh sách trung tâm)
-          <br />
-          2. Quý khách có thể thanh toán bằng hình thức chuyển khoản như sau:
-        </div>
-
-        <div style ={{backgroundColor: '#e6e6e6', width: '500px', height: '70px', borderRadius: '5px'}}>
-          Vui lòng chuyển khoản số tiền 0 VNĐ với nội dung như sau:
-          <br />
-          -
-        </div>
-
-        <h3 style={{fontWeight:'bold'}}>Thông tin đơn vị thụ hưởng</h3>
-
-        <h4>Ngân hàng Vietinbank</h4>
-        <li>Tên TK: Công Ty Cổ Phần Vacxin Việt Nam - Chi Nhánh Thành Phố Hồ Chí Minh</li>
-        <li>STK: <span style={{color:'red'}}>110610016789</span></li>
-        <li>Ngân hàng TMCP Công thương Việt Nam (Vietinbank) - Chi nhánh 2 TP.HCM</li>
-
-        <h4>Ngân hàng MBBank</h4>
-        <li>Tên TK: Công Ty Cổ Phần Vacxin Việt Nam - Chi Nhánh Thành Phố Hồ Chí Minh</li>
-        <li>STK: <span style={{color:'red'}}>1031101859009</span></li>
-        <li>Ngân hàng TMCP Quân Đội (MB Bank) - Chi nhánh Sở Giao Dịch 2 TP.HCM</li>
-
-        <div style={{paddingTop: '10px', paddingBottom: '10px'}}>
-          Lưu ý: Nếu quý khách không nhận được tin nhắn xác nhận đã thanh toán từ VNC trong vòng 48 giờ (Không tính thứ 7, chủ nhật) sau khi hoàn tất quá trình chuyển khoản, vui lòng liên hệ đến 
-          <span style={{color: 'orange'}}> hotline 02873006595</span> để được hỗ trợ.
-        </div>
-
-        <div style={{display: "inline-flex", paddingBottom: '10px'}}>
-          <div style={{paddingRight: '20px'}}>
-            <Button  variant="contained">
-              CHI TIẾT ĐƠN HÀNG
-            </Button>
+      <div style={{marginLeft: "200px"}}>
+          <h4>VNVC chỉ thực hiện giữ chỗ cho vắc xin sau khi nhận được thanh toán từ Quý khách và tổng số lượng đặt mua không vượt quá giới hạn mũi tiêm chúng tôi có thể cung cấp.</h4>
+          <div>
+            Mã đặt mua : {data?.MaDatMua}
           </div>
 
-          <Button  variant="contained" style={{backgroundColor: "orange"}}>
-            TIẾP TỤC ĐẶT HÀNG
-          </Button>
+          <div>
+            Số người đăng ký : {data?.NguoiTiem?.length}
+          </div>
 
+          <div>
+            Tổng số tiền cần thanh toán : {tongTien(data?.NguoiTiem).toLocaleString()} VNĐ
+          </div>
+
+          <h3 style = {{fontWeight: 'bold'}}>Hướng dẫn thanh toán</h3>
+
+          <div>
+            1. Quý khách có thể thanh toán trực tiếp tại các Trung tâm VNVC trên toàn quốc (Danh sách trung tâm)
+            <br />
+            2. Quý khách có thể thanh toán bằng hình thức chuyển khoản như sau:
+          </div>
+
+          <div style ={{backgroundColor: '#e6e6e6', width: '500px', height: '70px', borderRadius: '5px'}}>
+            Vui lòng chuyển khoản số tiền 0 VNĐ với nội dung như sau:
+            <br />
+            -
+          </div>
+
+          <h3 style={{fontWeight:'bold'}}>Thông tin đơn vị thụ hưởng</h3>
+
+          <h4>Ngân hàng Vietinbank</h4>
+          <li>Tên TK: Công Ty Cổ Phần Vacxin Việt Nam - Chi Nhánh Thành Phố Hồ Chí Minh</li>
+          <li>STK: <span style={{color:'red'}}>110610016789</span></li>
+          <li>Ngân hàng TMCP Công thương Việt Nam (Vietinbank) - Chi nhánh 2 TP.HCM</li>
+
+          <h4>Ngân hàng MBBank</h4>
+          <li>Tên TK: Công Ty Cổ Phần Vacxin Việt Nam - Chi Nhánh Thành Phố Hồ Chí Minh</li>
+          <li>STK: <span style={{color:'red'}}>1031101859009</span></li>
+          <li>Ngân hàng TMCP Quân Đội (MB Bank) - Chi nhánh Sở Giao Dịch 2 TP.HCM</li>
+
+          <div style={{paddingTop: '10px', paddingBottom: '10px'}}>
+            Lưu ý: Nếu quý khách không nhận được tin nhắn xác nhận đã thanh toán từ VNC trong vòng 48 giờ (Không tính thứ 7, chủ nhật) sau khi hoàn tất quá trình chuyển khoản, vui lòng liên hệ đến 
+            <span style={{color: 'orange'}}> hotline 02873006595</span> để được hỗ trợ.
+          </div>
+
+          <div style={{display: "inline-flex", paddingBottom: '10px'}}>
+            <div style={{paddingRight: '20px'}}>
+              <Button  variant="contained">
+                CHI TIẾT ĐƠN HÀNG
+              </Button>
+            </div>
+
+            <Button  variant="contained" style={{backgroundColor: "orange"}} onClick={backToVaccinePage}>
+              TIẾP TỤC ĐẶT HÀNG
+            </Button>
+
+          </div>
+          
+          <div style={{fontWeight:'bold'}}>Cảm ơn quý khách đã tin tưởng và sử dụng dịch vụ tại VNVC</div>
+
+          <div style={{color:'orange'}}>Hotline 02873006595</div>
         </div>
-        
-        <div style={{fontWeight:'bold'}}>Cảm ơn quý khách đã tin tưởng và sử dụng dịch vụ tại VNVC</div>
-
-        <div style={{color:'orange'}}>Hotline 02873006595</div>
 
       </div>
 
