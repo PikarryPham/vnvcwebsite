d753a9ee01f0d68b62663617766814a9150dd505
LONG La Hoang <85540732+hoanglongla@users.noreply.github.com>
2022-05-03 17:54:29 +0700

    Install Redis

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,20 +1,62 @@
 const Vaccine = require("../models/Vaccines");
+const client = require("../redis");
 
 const search = function (req, res) {
-  // console.log("~~abc");
-  // console.log(req.query.searchKey);
   var search = req.query.searchKey;
-
-  Vaccine.find(
-    // { Ten: { $regex: /${req.query.searchKey}/, $options: "i" } },
-    { Ten: new RegExp(search, "i") },
-    function (err, vaccines) {
-      // console.log("err", err);
-      // console.log("vaccines:", vaccines);
-      res.send(vaccines);
+  client.get("Vaccine" + search, async (err, data) => {
+    if (err) {
+      res.status(500);
     }
-  );
+    //if no match found
+    if (data != null) {
+      let vaccines = JSON.parse(data);
+      // console.log(vaccines);
+      res.send(vaccines);
+    } else {
+      Vaccine.find({ Ten: new RegExp(search, "i") }, function (err, vaccines) {
+        client.set("Vaccine" + search, JSON.stringify(vaccines));
+        res.send(vaccines);
+      });
+    }
+  });
 };
+
+// const search = function (req, res) {
+//   try {
+//     var searchKey = req.query.searchKey;
+
+//     // Check the redis store for the data first
+//     client.get(searchKey, async (err, result) => {
+//       if (result) {
+//         return res.status(200).send({
+//           error: false,
+//           message: `Result for ${searchKey} from the cache`,
+//           data: JSON.parse(result),
+//         });
+//       } else {
+//         // When the data is not found in the cache then we can make request to the server
+
+//         const result = await axios.get("http://localhost:5000/search", {
+//           params: {
+//             searchKey: searchKey,
+//           },
+//         });
+
+//         // save the record in the cache for subsequent request
+//         client.setex(searchKey, 1440, JSON.stringify(result.data.results));
+
+//         // return the result to the client
+//         return res.status(200).send({
+//           error: false,
+//           message: `Result for ${searchKey} from the server`,
+//           data: result.data.results,
+//         });
+//       }
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 module.exports = {
   search,
--- server/routes/register.js
+++ server/routes/register.js
@@ -1,25 +1,25 @@
-const express = require('express');
+const express = require("express");
 
-const registerController = require('../controllers/registerController');
-const diachiController = require("../controllers/diachiController")
-const vaccineController = require("../controllers/vaccineController")
-const customerController = require("../controllers/customerController")
+const registerController = require("../controllers/registerController");
+const diachiController = require("../controllers/diachiController");
+const vaccineController = require("../controllers/vaccineController");
+const customerController = require("../controllers/customerController");
 
-const checkCache = require("../middleware/redis")
+const checkCache = require("../middleware/redis");
 
 const router = express.Router();
 
-router.post('/add-infor',registerController.addInfor)
-router.post('/get-infor',checkCache,registerController.getInfor)
+router.post("/add-infor", registerController.addInfor);
+router.post("/get-infor", checkCache, registerController.getInfor);
 
-router.post('/get-province',diachiController.ListProvince)
-router.post('/get-district',diachiController.ListDistrict)
-router.post('/get-commune',diachiController.ListCommune)
+router.post("/get-province", diachiController.ListProvince);
+router.post("/get-district", diachiController.ListDistrict);
+router.post("/get-commune", diachiController.ListCommune);
 
-router.post('/get-vaccine-center',vaccineController.ListVaccineCenter)
-router.post('/get-vaccine-province',vaccineController.ListProvince)
-router.post('/get-vaccines',vaccineController.ListVaccines)
+router.post("/get-vaccine-center", vaccineController.ListVaccineCenter);
+router.post("/get-vaccine-province", vaccineController.ListProvince);
+router.post("/get-vaccines", vaccineController.ListVaccines);
 
-router.post("/get-customer",checkCache,customerController.GetCustomer)
+router.post("/get-customer", checkCache, customerController.GetCustomer);
 
 module.exports = router;
